1253c21cfc55211ca6bc82954d88f073f713fd98
stinightfall <jeremycader@gmail.com>
2019-12-12 09:37:57 -0800

    gamification

--- lib/screens/game_signoffs.dart
+++ lib/screens/game_signoffs.dart
@@ -17,78 +17,75 @@ class _GamificationSignoffState extends State<GamificationSignoff> {
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             Row(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(
-                  left: 50,
-                  top: 20,
-                  bottom: 20,
-                  right: 50,
+              Expanded(
+                child: CircleAvatar(
+                  child: Text('1'),
+                  backgroundColor: Color(0xffEF8D69F),
                 ),
-                child: Flex(
-                    direction: Axis.horizontal,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      CircleAvatar(
-                        child: Text('1'),
-                        backgroundColor: Color(0xffEF8D69F),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 20),
-                      ),
-                      Image.asset('assets/stevie.png',
-                          //fit: BoxFit.fill,
-                          //height: 40,
-                          width: 65,
-                          fit: BoxFit.fitWidth),
-                      Padding(
-                        padding: EdgeInsets.only(left: 20),
-                      ),
-                      Text(
-                        'Stevie',
-                        style: TextStyle(fontSize: 18),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 20),
-                      ),
-                      Text(
-                        '1000',
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    ]),
               ),
-            ]),
-            Row(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
-              Padding(
-                padding:
-                    EdgeInsets.only(left: 50, right: 50, top: 20, bottom: 20),
-                child: Flex(
-                    direction: Axis.horizontal,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      CircleAvatar(
-                        child: Text('2'),
-                        backgroundColor: Color(0xffEF8D69F),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 20),
-                      ),
-                      Image.asset('assets/twila.png',
-                          //fit: BoxFit.fill,
-                          //height: 40,
-                          width: 65,
-                          fit: BoxFit.fitWidth),
-                      Padding(
-                        padding: EdgeInsets.only(left: 20),
-                      ),
-                      Text('Twi', style: TextStyle(fontSize: 18)),
-                      Padding(
-                        padding: EdgeInsets.only(left: 20),
-                      ),
-                      Text('1', style: TextStyle(fontSize: 18)),
 
-                    ]),
+              Expanded(
+                child: Image.asset('assets/stevie.png',
+                    //fit: BoxFit.fill,
+                    //height: 40,
+                    width: 65,
+                    fit: BoxFit.fitWidth),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20),
+              ),
+              Expanded(
+                child: Text(
+                  'Stevie',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20),
+              ),
+              Expanded(
+                child: Text(
+                  '1000',
+                  style: TextStyle(fontSize: 18),
+                ),
               ),
             ]),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Expanded(
+                    child: CircleAvatar(
+                      child: Text('1'),
+                      backgroundColor: Color(0xffEF8D69F),
+                    ),
+                  ),
+
+                  Expanded(
+                    child: Image.asset('assets/stevie.png',
+                        //fit: BoxFit.fill,
+                        //height: 40,
+                        width: 65,
+                        fit: BoxFit.fitWidth),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 20),
+                  ),
+                  Expanded(
+                    child: Text(
+                      'Stie',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 20),
+                  ),
+                  Expanded(
+                    child: Text(
+                      '00',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ]),
           ],
         ),
       ),
